5cb476671dc1e876e92041a676a385a3e2b11b66
Vernitra Jones <vernitrasmedia@gmail.com>
2022-07-11 16:54:02 -0500

    commit tree 9889a52c88bff13d90ae3e658eefa9f6dec24675

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let cost
+  if (distance <= 400) {
+    cost = 'This one is on me!';
+  }
+  else if (distance > 400 && distance < 2000) {
+    cost = 'That will be twenty bucks.';
+  }
+  else if (distance > 2000 && distance < 2500) {
+    cost = 'I will gladly take your thirty bucks.';
+  }
+  else if (distance > 2500) {
+    cost = 'No can do.';
+  }
+  return cost
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let travelingOpinion
+  city === 'NYC' ? travelingOpinion = 'Ok, sounds good.': travelingOpinion = 'No go.'
+
+  return travelingOpinion
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.' 
+  }
 }
